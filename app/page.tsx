9097c1f78d18d68c676198ed19a72e3bdb841f43
JCKing97 <jck100notout@hotmail.co.uk>
2026-05-21 21:05:11 +0100

    Third attempt at fixing routing of components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Card from '../components/Card';
-import Footer from '../components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Card from './components/Card';
+import Footer from './components/Footer';
 
 export default function Home() {
   const featuredCollections = [
