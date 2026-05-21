5c91f3bc7cc71838d357c8b7f9f2c3e39c5d1f31
JCKing97 <jck100notout@hotmail.co.uk>
2026-05-21 22:14:48 +0100

    More playing around with styling

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -4,23 +4,10 @@ import styles from './Header.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      {/* <div className={styles['header-top']}>
-        <div className={styles.container}>
-          <span>Welcome to the Natural History Museum</span>
-          <div>
-            <Link href="#" className={styles.link}>Sign in</Link> |
-            <Link href="#" className={styles.link}>Help</Link>
-          </div>
-        </div>
-      </div> */}
       <div className={styles['header-main']}>
         <div className={styles.container}>
           <div className={styles.logo}>
-            <img
-              src="https://www.nhm.ac.uk/content/dam/nhmwww/about-us/logo-nhm.svg"
-              alt="Natural History Museum"
-              width={150}
-            />
+            <Link href="/" className={styles.homelink}>Natural History Museum</Link>
           </div>
           <nav className={styles['header-nav']}>
             <Link href="#" className={styles.link}>Visit</Link>
