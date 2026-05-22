8d9875826a684a25ab01c8005a44a7ba2b7c870b
JCKing97 <jck100notout@hotmail.co.uk>
2026-05-22 18:20:55 +0100

    Try adding div to fix GameBoard layout

--- app/components/GameBoard/GameBoard.tsx
+++ app/components/GameBoard/GameBoard.tsx
@@ -3,24 +3,26 @@ import GameCard from '@/components/GameCard';
 
 export default function GameBoard() {
   return (
-    <div className={styles['game-board-controls']}>
-        <h1>Jurassic Jumble</h1>
-        <div class={styles['game-board-controls-buttons']}>
-            <div class={styles['game-board-controls-button']}>
-                <Link href="#" className="btn">New Cards</Link>
-            </div>
-            <div class={styles['game-board-controls-button']}>
-                <Link href="#" className="btn">Snap!</Link>
+    <div>
+        <div className={styles['game-board-controls']}>
+            <h1>Jurassic Jumble</h1>
+            <div class={styles['game-board-controls-buttons']}>
+                <div class={styles['game-board-controls-button']}>
+                    <Link href="#" className="btn">New Cards</Link>
+                </div>
+                <div class={styles['game-board-controls-button']}>
+                    <Link href="#" className="btn">Snap!</Link>
+                </div>
             </div>
         </div>
-    </div>
-    <div className={styles.gameboard}>
-      <div className={styles['game-board-card-container']}>
-        <GameCard title="Tyrannosaurus Rex" description="Monstrous carnivore!" />
-      </div>
-      <div className={styles['game-board-card-container']}>
-        <GameCard title="Diplodocus" description="Giant herbivore!" />
-      </div>
+        <div className={styles.gameboard}>
+        <div className={styles['game-board-card-container']}>
+            <GameCard title="Tyrannosaurus Rex" description="Monstrous carnivore!" />
+        </div>
+        <div className={styles['game-board-card-container']}>
+            <GameCard title="Diplodocus" description="Giant herbivore!" />
+        </div>
+        </div>
     </div>
   );
 }
